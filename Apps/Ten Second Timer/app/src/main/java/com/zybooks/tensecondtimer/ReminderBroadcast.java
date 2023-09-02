05b8f777b686c176e73c4fad5c044aa09b60b8a0
adamsissoko <108204686+adamsissoko@users.noreply.github.com>
2023-09-02 13:39:06 -0400

    Add files via upload

---
+++ Apps/Ten Second Timer/app/src/main/java/com/zybooks/tensecondtimer/ReminderBroadcast.java
@@ -0,0 +1,24 @@
+package com.zybooks.tensecondtimer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class ReminderBroadcast extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notifyText")
+                .setSmallIcon(R.drawable.ic_android_black_24dp)
+                .setContentTitle("Reminder")
+                .setContentText("This is a reminder...")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(200, builder.build());
+
+    }
+}
